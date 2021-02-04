3c66963fe07a5ec72145f7db5f3fb0fa2754b4c3
Solomon Swager <zitiswager@gmail.com>
2021-02-04 01:02:55 -0500

    edited urban dictionary

--- commands/ub.js
+++ commands/ub.js
@@ -1,6 +1,7 @@
 const Discord = require ("discord.js");
 const { version, runnerPicture } = require('../config.json');
 const querystring = require('querystring');
+const fetch = require("node-fetch");
 
 //exports whatever is below to the index.js file
 module.exports = {
@@ -13,16 +14,19 @@ module.exports = {
     //when called execute this message
 	execute(message, args) {
 
+        console.log("hello1")
         if (!args.length) {
-            return message.channel.send('You need to give me something to search for. Example \`!ub retard\`.')
+            return message.channel.send('You need to give me something to search for. Example \`!ub Urban Dictionary\`.')
         }
+        console.log("hello2")
         const query = querystring.stringify({ term: args.join(' ') });
-
-        const { list } = await fetch(`https://api.urbandictionary.com/v0/define?${query}`).then(response => response.json());
-
+        console.log("hello3")
+        const { list } = fetch(`https://api.urbandictionary.com/v0/define?${query}`).then(response => response.json());
+        console.log(list)
         if (!list.length) {
             return message.channel.send(`No results found for **${args.join(' ')}**.`);
-        }        
+        }
+        console.log("hello5")
         //what the message does
         const pingEmbed = new Discord.MessageEmbed()
                 .setColor('#0099ff')
